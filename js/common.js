f6c7b0d1bb11a12de98141d26b3075818d8a90d0
chibariyo-inafuku <inafuku@chibariyo.okinawa>
2021-11-26 08:55:23 +0900

    ・OGイメージタグ追加 ・フェードインアニメーション追加

--- js/common.js
+++ js/common.js
@@ -1,6 +1,22 @@
 //共通パーツ読み込み
- $(function() {
- $("#header").load("header.html");
- $("#footer").load("footer.html");
- $("#copy").load("copy.html");
+$('.in-right').on('inview', function(event, isInView) {
+  if (isInView) {
+    $(this).addClass('animate__animated animate__bounceInRight');
+  } else {
+    //$(this).removeClass('animate__animated animate__bounceInRight');
+  }
+});
+$('.in-left').on('inview', function(event, isInView) {
+  if (isInView) {
+    $(this).addClass('animate__animated animate__bounceInLeft');
+  } else {
+    //$(this).removeClass('animate__animated animate__bounceInRight');
+  }
+});
+$('.in-down').on('inview', function(event, isInView) {
+  if (isInView) {
+    $(this).addClass('animate__animated animate__bounceInDown');
+  } else {
+    //$(this).removeClass('animate__animated animate__bounceInRight');
+  }
 });
